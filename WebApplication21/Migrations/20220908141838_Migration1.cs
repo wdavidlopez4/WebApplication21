7241ef3e47b0cf2b30bd807171d1b9a0f4494bdf
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2022-09-08 09:28:11 -0500

    migracion y vistas

---
+++ WebApplication21/Migrations/20220908141838_Migration1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication21.Migrations
+{
+    public partial class Migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Escuelas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Escuelas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estudiantes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DocumentoIdentidad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaDeNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EscuelaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiantes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Estudiantes_Escuelas_EscuelaId",
+                        column: x => x.EscuelaId,
+                        principalTable: "Escuelas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Estudiantes_EscuelaId",
+                table: "Estudiantes",
+                column: "EscuelaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estudiantes");
+
+            migrationBuilder.DropTable(
+                name: "Escuelas");
+        }
+    }
+}
--- WebApplication21/Program.cs
+++ WebApplication21/Program.cs
@@ -1,14 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using WebApplication21.Data;
+using WebApplication21.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//IOC
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddScoped<EscuelaServices>();
+
+//config de db
 builder.Services.AddDbContext<WebApplication21DbContext>(options => 
     options.UseSqlServer(
-        builder.Configuration.GetConnectionString("")));
+        builder.Configuration.GetConnectionString("WebConnectionString")));
 
 var app = builder.Build();
 
